1a305220835b002e9a782a1d9d3e11ba23c82333
Alishen <alisene@gmail.com>
2025-09-25 12:24:32 +0200

    formatting + drawable moved into res folder

--- app/src/main/java/de/berlindroid/zepatch/patchable/Ziva.kt
+++ app/src/main/java/de/berlindroid/zepatch/patchable/Ziva.kt
@@ -17,26 +17,28 @@ import de.berlindroid.zepatch.ui.SafeArea
 @Patch("Ziva") // you need this to register it
 @Composable
 fun Ziva(
-  shouldCapture: Boolean = false, // used to activate the convert to bitmap
-  onBitmap: (ImageBitmap) -> Unit = {}, // used to return the bitmap from the SafeArea
+    shouldCapture: Boolean = false, // used to activate the convert to bitmap
+    onBitmap: (ImageBitmap) -> Unit = {}, // used to return the bitmap from the SafeArea
 ) {
-  // Safe Area is the part that becomes the patch
-  SafeArea(
-    shouldCapture = shouldCapture, // You need to pass this through from the parent or it won't work
-    onBitmap = onBitmap, // You need to pass this through from the parent or it won't work
-  ) {
-    Image(
-      painter = painterResource(R.drawable.kura),
-      modifier = Modifier.size(200.dp).fillMaxSize(),
-      contentScale = ContentScale.FillHeight,
-      contentDescription = null
-    )
-  }
-  // Add interactive pieces here. This will not be part of the patch
+    // Safe Area is the part that becomes the patch
+    SafeArea(
+        shouldCapture = shouldCapture, // You need to pass this through from the parent or it won't work
+        onBitmap = onBitmap, // You need to pass this through from the parent or it won't work
+    ) {
+        Image(
+            painter = painterResource(R.drawable.kura),
+            modifier = Modifier
+              .size(200.dp)
+              .fillMaxSize(),
+            contentScale = ContentScale.FillHeight,
+            contentDescription = null
+        )
+    }
+    // Add interactive pieces here. This will not be part of the patch
 }
 
 @Preview
 @Composable
-fun ZivaPreview(){
-  Ziva()
+fun ZivaPreview() {
+    Ziva()
 }
