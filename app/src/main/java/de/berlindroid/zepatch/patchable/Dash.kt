36086ffb55380f63dc8a3490aa80467f8b4e84d7
Muritz <moritz.brandscheid@alte-leipziger.de>
2025-09-26 13:16:17 +0200

    revert

---
+++ app/src/main/java/de/berlindroid/zepatch/patchable/Dash.kt
@@ -0,0 +1,48 @@
+package de.berlindroid.zepatch.patchable
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import de.berlindroid.zepatch.R
+import de.berlindroid.zepatch.annotations.Patch
+import de.berlindroid.zepatch.ui.SafeArea
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Patch("MoritzDash")
+@Composable
+fun Dash(
+    shouldCapture: Boolean = false,
+    onBitmap: (ImageBitmap) -> Unit = {},
+) {
+    SafeArea(
+        shouldCapture = shouldCapture,
+        onBitmap = onBitmap,
+    ) {
+        Box(
+            Modifier.background(Color.White).padding(16.dp)
+        ) {
+            Image(
+                modifier = Modifier.size(200.dp),
+                painter = painterResource(R.drawable.dash),
+                contentDescription = null
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+private fun DashPrev() {
+    Dash {  }
+}
