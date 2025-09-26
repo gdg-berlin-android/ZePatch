16afbdbcc73d5c97b0940cab3d780f2c2c0df8c6
ZeBadge <zebadge-gdg@gmail.com>
2025-09-26 11:04:47 +0200

    Add FirefoxMomo border

--- app/src/main/java/de/berlindroid/zepatch/patchable/FirefoxMomoLogo.kt
+++ app/src/main/java/de/berlindroid/zepatch/patchable/FirefoxMomoLogo.kt
@@ -1,12 +1,19 @@
 package de.berlindroid.zepatch.patchable
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import de.berlindroid.zepatch.R
 import de.berlindroid.zepatch.annotations.Patch
@@ -14,6 +21,7 @@ import de.berlindroid.zepatch.ui.SafeArea
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Patch("FirefoxMomoLogo")
+@Preview
 @Composable
 fun FirefoxMomoLogo(
     shouldCapture: Boolean = false,
@@ -23,10 +31,17 @@ fun FirefoxMomoLogo(
         shouldCapture = shouldCapture,
         onBitmap = onBitmap,
     ) {
-        Image(
-            modifier = Modifier.size(200.dp),
-            painter = painterResource(R.drawable.ic_momo),
-            contentDescription = null
-        )
+        Box(contentAlignment = Alignment.Center,
+            modifier = Modifier
+            .border(2.dp, Color.Black, shape = CircleShape)
+                .padding(start = 2.dp, end = 6.dp)
+            .size(218.dp)
+        ) {
+            Image(
+                modifier = Modifier.size(200.dp),
+                painter = painterResource(R.drawable.ic_momo),
+                contentDescription = null
+            )
+        }
     }
 }
